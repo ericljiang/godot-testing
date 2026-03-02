00ac444f7c05a59d1ddfc145a3040bf31a7cfac9
Eric Jiang <eric.jiang2@gmail.com>
2026-03-01 23:05:52 -0700

    Add surfing surfaces and show speed as percentage

--- speedometer.gd
+++ speedometer.gd
@@ -1,6 +1,6 @@
 extends Label
 
-@export var player: CharacterBody3D
+@export var player: Player
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -12,10 +12,11 @@ func _process(_delta: float) -> void:
 	
 	
 	Speed:        %.2f m/s
-	Ground speed: %.2f m/s
+	Ground speed: %.2f m/s (%3.0f%%)
 	Velocity:     (%5.2f, %5.2f)""" % [
 		velocity.length(),
 		Vector2(velocity.x, velocity.z).length(),
+		Vector2(velocity.x, velocity.z).length() / player.MAX_GROUND_SPEED * 100,
 		velocity.x,
 		velocity.z,
 	]
